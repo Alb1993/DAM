987289e49300b5809e6ea6f57c5048b2ddf4f7fd
Alb1993 <99869522+Alb1993@users.noreply.github.com>
2022-05-06 10:51:09 +0200

    Update and rename ej1_modular.java to ej1_HolaMundo.java

--- Portfolio/Modulos/M03-Programacion/UF2/UF2Ejercicios/ej1_HolaMundo.java
+++ Portfolio/Modulos/M03-Programacion/UF2/UF2Ejercicios/ej1_HolaMundo.java
@@ -1,8 +1,11 @@
 package Uf2modular;
 
 
-public class ej1_modular {
-
+public class ej1_HolaMundo {
+    /*
+    Ex01_HolaMundo
+    Generar un mètode que, en cridar-lo, aquest pinti per pantalla un "Hola mundo"
+    */
     
     public static void main(String[] args) {
         
