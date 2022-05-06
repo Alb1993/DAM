332b6544dc2d74921c3eb6824fe5765090994e15
Alb1993 <99869522+Alb1993@users.noreply.github.com>
2022-05-06 11:47:05 +0200

    Update ExamenUF2Ex02.java

--- Portfolio/Modulos/M03-Programacion/UF2/ExamenUF2/ExamenUF2Ex02.java
+++ Portfolio/Modulos/M03-Programacion/UF2/ExamenUF2/ExamenUF2Ex02.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 
 
 public class ExamenUF2Ex02 {
+    /*
+    Amb el llistat de l’exemple anterior, fes un array de String que contingui els
+    diferents noms amb els seus cognoms tal i com apareixen al llistat.
+        String[] nomsComplets = { “Torres Hernández, Pere”, “Majoral Gatell, Anna”, … };
+    A continuació, crea els mètodes:
+        static void ordenaBombolla (String[] nomsComplets) i
+        static void mostraNoms(String[] nomsComplets)
+    El mètode ordenaBombolla() ordenarà el llistat per nom.
+    El mètode mostraNoms() donarà la següent sortida un cop la llista estigui
+    ordenada
+    
+    b) Fes un mètode anomenat:
+    static int cercaBinaria(String[] nomsComplets, String nom)
+    que, a partit de la llista anterior ordenada per noms de pila, trobi si un cert nom de
+    pila hi és o no a la llista. En cas de ser-hi, mostrarà la seva posició, en cas contrari
+    dirà que no hi és.
+
+    
+    */
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
